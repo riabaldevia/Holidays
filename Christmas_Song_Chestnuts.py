d644b04375e97adf47c0f017cc88e1c077ecbb5c
Ria Baldevia <30583413+riabaldevia@users.noreply.github.com>
2017-12-10 23:03:24 -0500

    Update Christmas_Song_Chestnuts.py

--- Christmas_Song_Chestnuts.py
+++ Christmas_Song_Chestnuts.py
@@ -2,4 +2,6 @@ import pyknon
 B4, C4, B4, C4, D4, C4 D4 E4 #Unrecognizable line
 Eb4 Eb5 D5 C5 Bb5 Ab5 G4 G4 G4 
 Eb4 C5 Bb5 Ab5 G4 F4 Eb4
-Eb4 Eb4 F4 F4 Eb4 G4 Bb5 C5 D5
+Eb4 Eb4 F4 F4 Eb4 G4 Bb5 C5 D5 C5
+B5 B5 Db5 B5 Bb5 Ab5 
+Bb5 Bb5 Ab5 G4 F4
